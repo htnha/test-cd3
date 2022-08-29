6a0fe65f3daf8d307041a3100158688f6d698b22
Tuan010101 <90946207+Tuan010101@users.noreply.github.com>
2022-08-29 09:05:09 +0700

    Update HelloWorld.java
    Tuấn

--- test-cd3/src/HelloWorld.java
+++ test-cd3/src/HelloWorld.java
@@ -13,4 +13,6 @@ public class HelloWorld {
         System.out.println("19/8/2001");
         System.out.println("4. Nguyễn Đào Thành Đạt");
         System.out.println("16/6/2001");
+        System.out.println("5. Nguyễn Ngọc Tuấn");
+        System.out.println("01/01/2001");
 }

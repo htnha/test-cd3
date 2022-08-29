9365fd1fc0dfab873c89aa6a20d5fef0d7086d59
hoangthang870 <hoangthangdnd870@gmail.com>
2022-08-29 09:03:20 +0700

    commit

--- test-cd3/src/HelloWorld.java
+++ test-cd3/src/HelloWorld.java
@@ -11,5 +11,7 @@ public class HelloWorld {
         System.out.println("12/10/2001");
         System.out.println("3. Hoàng Trung Phong");
         System.out.println("19/8/2001");
+        System.out.println("3. Hoàng Trung Phong");
+        System.out.println("19/8/2001");
     }
 }

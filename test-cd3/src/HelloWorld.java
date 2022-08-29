61198c2f91a7277533dc12f4296bd21578c42a00
htnha <hoang.tuan.nha@gmail.com>
2022-08-29 08:01:41 +0700

    Thêm dòng code comment

--- test-cd3/src/HelloWorld.java
+++ test-cd3/src/HelloWorld.java
@@ -2,5 +2,7 @@ public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Xin chao lop CNTT K61");
         System.out.println("Tạo commit thứ 2");
+
+        //Mỗi bạn thêm dòng code in Họ và tên + Ngày sinh
     }
 }

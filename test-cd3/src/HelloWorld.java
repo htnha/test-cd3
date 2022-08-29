6ff586a4bb220fe65c57e60050db1f251863e192
htnha <hoang.tuan.nha@gmail.com>
2022-08-29 07:56:16 +0700

    Tạo file đầu tiên

--- test-cd3/src/HelloWorld.java
+++ test-cd3/src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main(String[]args){
         System.out.println("Xin chao lop CNTT K61");
+        System.out.println("Tạo commit thứ 2");
     }
 }
